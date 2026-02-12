b6bdf8a5ddb2e120757258ac5ddb0f259b818f18
ritwiksaraf <ritwiksaraf123@gmail.com>
2026-02-11 21:33:50 -0600

    Quartz sync: Feb 11, 2026, 9:33 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "युयुत्सवः",
+    pageTitle: "ritwiksaraf.com",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
